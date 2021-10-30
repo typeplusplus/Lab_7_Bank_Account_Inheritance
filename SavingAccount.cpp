df6f045705a8bd264ca4cdce919ca776b18c947f
turn.exe <80058678+typeplusplus@users.noreply.github.com>
2021-10-30 00:31:31 -0700

    Add files via upload

--- SavingAccount.cpp
+++ SavingAccount.cpp
@@ -6,3 +6,10 @@
 
 using namespace std;
 
+SavingAccount::SavingAccount()
+{
+    interest_rate = 0;
+    min_balance = 0;
+}
+
+SavingAccount::SavingAccount(string acctNum, double b, double i): BankAccount(acct)
